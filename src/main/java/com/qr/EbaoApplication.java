292abe34dad81f836505544313585f8e65b64309
诗 <347757235@qq.com>
2020-11-09 10:24:50 +0800

    mybatis配置

--- src/main/java/com/qr/EbaoApplication.java
+++ src/main/java/com/qr/EbaoApplication.java
@@ -1,10 +1,12 @@
 package com.qr;
 
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan(basePackages = "com.qr.dao")
 public class EbaoApplication {
 
     public static void main(String[] args) {
