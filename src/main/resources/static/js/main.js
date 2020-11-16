c907efed01ea2b650a449c30647e83d2a78ef31b
MrXiak <2410965953@qq.com>
2020-11-16 08:37:58 +0800

    .

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,51 +1,47 @@
-layui.use(['element'], function(){
-	$ = layui.jquery;
-  	element = layui.element();
-  
-  //导航的hover效果、二级菜单等功能，需要依赖element模块
-  // larry-side-menu向左折叠
-  	if($(window).width()<750){
-		trun = 0;
-	}else{
-		trun = 1;
-	}
-	
-$('.larry-side-menu').click(function() {
-  if(trun){
-			$('.x-side').animate({left: '0px'},200).siblings('.x-main').animate({left: '220px'},200);
-			trun=0;
-		}else{
-			$('.x-side').animate({left: '-220px'},200).siblings('.x-main').animate({left: '0px'},200);
-			trun=1;
-		}
+$(function () {
 
-});
+  'use strict';
 
+  var $distpicker = $('#distpicker');
 
-
-  	//监听导航点击
-  	element.on('nav(side)', function(elem){
-    	title = elem.find('cite').text();
-    	url = elem.find('a').attr('_href');
-    	// alert(url);
-
-    	for (var i = 0; i <$('.x-iframe').length; i++) {
-    		if($('.x-iframe').eq(i).attr('src')==url){
-    			element.tabChange('x-tab', i);
-    			return;
-    		}
-    	};
-
-    	res = element.tabAdd('x-tab', {
-	        title: title//用于演示
-	        ,content: '<iframe frameborder="0" src="'+url+'" class="x-iframe"></iframe>'
-		    });
-
-
-		element.tabChange('x-tab', $('.layui-tab-title li').length-1);
-
-    	$('.layui-tab-title li').eq(0).find('i').remove();
+  $distpicker.distpicker({
+    province: '福建省',
+    city: '厦门市',
+    district: '思明区'
   });
+
+  $('#reset').click(function () {
+    $distpicker.distpicker('reset');
+  });
+
+  $('#reset-deep').click(function () {
+    $distpicker.distpicker('reset', true);
+  });
+
+  $('#destroy').click(function () {
+    $distpicker.distpicker('destroy');
+  });
+
+  $('#distpicker1').distpicker();
+
+  $('#distpicker2').distpicker({
+    province: '---- 所在省 ----',
+    city: '---- 所在市 ----',
+    district: '---- 所在区 ----'
+  });
+
+  $('#distpicker3').distpicker({
+    province: '浙江省',
+    city: '杭州市',
+    district: '西湖区'
+  });
+
+  $('#distpicker4').distpicker({
+    placeholder: false
+  });
+
+  $('#distpicker5').distpicker({
+    autoSelect: false
+  });
+
 });
-
-
