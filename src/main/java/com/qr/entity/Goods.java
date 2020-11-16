4702742fbf2c8be0629792ff8d9a367e8a118797
MrXiak <2410965953@qq.com>
2020-11-16 14:46:19 +0800

    .

--- src/main/java/com/qr/entity/Goods.java
+++ src/main/java/com/qr/entity/Goods.java
@@ -1,8 +1,10 @@
 package com.qr.entity;
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
 public class Goods {
     private String goodsId;
     private String goodsName;
